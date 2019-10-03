818d86439df74364cdd28b15be33f71ff316e766
Loger Nam <nam.pt@neo-lab.vn>
2019-10-03 08:47:21 +0700

    Clear Source

--- src/pages/Admin/Project.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-class Project extends React.Component{
-  render() {
-    return <h1 className="text-center">Project Page</h1>
-  }
-}
-
-export default Project
--- src/pages/Admin/User.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-class User extends React.Component{
-  render() {
-    return <h1 className="text-center">User Page</h1>
-  }
-}
-
-export default User
